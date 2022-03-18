9300302dfd0ef069b9cc4efd32cae69e07eacdd7
Muhammed Akgun <muhammeda@twentythree.com.tr>
2022-03-18 10:06:04 +0300

    fix: loader twig include in options

--- controller/options.php
+++ controller/options.php
@@ -3,6 +3,7 @@
 require_once '../vendor/autoload.php';
 require '../model/database.php';
 require 'sessionopt.php';
-
+$loader = new \Twig\Loader\FilesystemLoader('../view');
+$twig = new \Twig\Environment($loader);
 
 ?>

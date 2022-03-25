515946cec069f66f3d7231400ddaee47f0003274
Muhammed Akgun <muhammeda@twentythree.com.tr>
2022-03-25 09:40:22 +0300

    fix:sql query and desing and get method edit

--- controller/library.php
+++ controller/library.php
@@ -4,15 +4,38 @@ require 'options.php';
 
 $library = $twig->load('library.twig');
 
-$sql = "SELECT id,uyeid,namebook ,authorname,booktype,bookimage FROM books";
-$result = $conn->query($sql);
 
-  
+if(isset($_POST['query'])){
 
-        $book= [];
-    while($row = $result->fetch_assoc()) {
-        $book[]=$row;
-       
+    $name=$_POST['query'];
+    $search = "SELECT * FROM books WHERE namebook LIKE '%$name%'";
+    $results = $conn->query($search);
    
+       $book=[];
+   while($rows = $results->fetch_assoc()) {
+        $book[]=$rows;
+        
+}
+
+
+echo $library->render(['library'=>$book]);
+
+
+}else{
+    $sql = "SELECT id,uyeid,namebook ,authorname,booktype,bookimage FROM books";
+    $result = $conn->query($sql);
+    
+           $book=[];
+        while($row = $result->fetch_assoc()) {
+            $book[]=$row;
+           
+       
+    }
+   
+
+
+    
 }
 echo $library->render(['library'=>$book]);
+
+
--- controller/libraryuser.php
+++ controller/libraryuser.php
@@ -6,12 +6,15 @@ $libraryuser = $twig->load('libraryuser.twig');
 $bookid=$_GET['book'];
 $userid=$_GET['user'];
 
+
 $sql = "SELECT id,namebook ,authorname,booktype,bookimage FROM books WHERE id='$bookid'";
 $result = $conn->query($sql);
 
     while($row = $result->fetch_assoc()) {
         $book=$row;
+    
 }
+
 $sqluser = "SELECT id,name,surname,mail FROM users WHERE id='$userid'";
 $resultuser = $conn->query($sqluser);
 
@@ -20,4 +23,11 @@ $resultuser = $conn->query($sqluser);
      
    
 }
-echo $libraryuser->render(['library'=>$book,'user'=>$user]);
+$sqluserook = "SELECT id,uyeid,namebook ,authorname,booktype,bookimage FROM books WHERE uyeid='$userid'";
+$resultuserook = $conn->query($sqluserook);
+$bookuserook=[];
+    while($rowuserook = $resultuserook->fetch_assoc()) {
+        $bookuserook[]=$rowuserook;
+
+}
+echo $libraryuser->render(['library'=>$book,'user'=>$user,'userbook'=>$bookuserook]);
